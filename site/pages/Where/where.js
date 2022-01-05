1fd381e3385c7d431bed46ceb4d55695771d56e6
Loloweb <lowgau@gmail.com>
2022-01-05 11:13:14 +0100

    + d'entrainement js

--- site/pages/Where/where.js
+++ site/pages/Where/where.js
@@ -24,3 +24,24 @@ function initMap() {
         map: map2,
     });
   }
+
+$(function() { // premier entrainement javascript
+    
+    var $rota = $('.spin')
+    
+    $rota.click(function(){
+        var $this = $(this);
+        
+        if($this.data('rotating')){
+            clearInterval($this.data('rotating'));
+            $this.data('rotating', false)
+        } else {
+            $this.data('rotating', setInterval(function(){
+                var degree = $this.data('degree') || 0;
+                $this.css({ transform: 'rotate(' + degree + 'deg)'});
+                $this.data('degree', ++degree)
+            }, 5));
+        }
+    });
+    
+});
